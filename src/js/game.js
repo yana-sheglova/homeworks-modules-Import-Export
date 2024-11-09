e2554edc78101dc1fd0c5e78137ab56ddfda8c72
Yana Sheglova <yana.shheglova.98@bk.ru>
2024-11-09 10:56:14 +0300

    Task2 dane

---
+++ src/js/game.js
@@ -0,0 +1,16 @@
+export default class Game {
+    start() {
+      console.log('game started');
+    }
+};
+  
+export class GameSavingData {
+};
+  
+export function readGameSaving() {
+};
+  
+export function writeGameSaving() {
+};
+
+import Character from './domain.js';
